251379d42b9e5c53c5d04488b9092e9cd955d594
Siyu Sun <sunsiyu.tud@gmail.com>
2015-11-19 17:08:50 +0100

    add generic rmd report contains: - status plot - boxplot - hist - timeseries plot

---
+++ R/demos.R
@@ -0,0 +1,12 @@
+# # multiple process names, multiple flags
+# df <- data.frame(name = c("name1", "name2", "name3"),
+#                  start = c("20150101", "20150201", "20150301"),
+#                  end = c("20150110", "20150220", "20150330"),
+#                  flag = c("good", "med", "bad"))
+# statusplot(df)
+# # single process name, multiple flags
+# df <- data.frame(name = c("name1", "name1", "name1"),
+#                  start = c("20150101", "20150201", "20150301"),
+#                  end = c("20150110", "20150220", "20150330"),
+#                  flag = c("good", "med", "bad"))
+# statusplot(df)
--- R/tools.R
+++ R/tools.R
@@ -1,3 +1,10 @@
+#' Get File Extention
+#'
+#' @param path character of length 1
+#' @return character of file extension
+#' @examples
+#' fileext <- getfileext("example.R")
+#' @export
 getfileext <- function(path) {
   stopifnot(is.character(path))
   splitpath <- strsplit(path, split="\\.")[[1]]
@@ -7,16 +14,19 @@ getfileext <- function(path) {
     return(splitpath[length(splitpath)])
 }
 
-# multiple process names, multiple flags
-df <- data.frame(name = c("name1", "name2", "name3"),
-                 start = c("20150101", "20150201", "20150301"),
-                 end = c("20150110", "20150220", "20150330"),
-                 flag = c("good", "med", "bad"))
-# single process name, multiple flags
-df <- data.frame(name = c("name1", "name1", "name1"),
-                 start = c("20150101", "20150201", "20150301"),
-                 end = c("20150110", "20150220", "20150330"),
-                 flag = c("good", "med", "bad"))
+#' Create Process Status Plot
+#'
+#' Create process status plot, with each bar indicating each process, and each
+#' color indicating different categories of status.
+#'
+#' @param df a data.frame
+#' @return Invisible NULL
+#' @examples
+#' statusplot(data.frame(name = c("name1", "name2", "name3"),
+#'  start = c("20150101", "20150201", "20150301"),
+#'  end = c("20150110", "20150220", "20150330"),
+#'  flag = c("good", "med", "bad")))
+#' @export
 statusplot <- function(df)
 {
   df$name <- as.factor(df$name)
@@ -29,15 +39,18 @@ statusplot <- function(df)
   df$start <- as.Date(df$start, "%Y%m%d")
   df$end <- as.Date(df$end, "%Y%m%d")
   df$flag <- ordered(df$flag, levels=c("good", "med", "bad"))
+
   p <- ggplot(df, aes(xmin = as.Date(start),
                       xmax = as.Date(end),
                       ymin = ymin,
                       ymax = ymin + 0.9,
                       fill = factor(flag)))
-  p + geom_rect() +
+  p <- p + geom_rect() +
     scale_x_date(breaks = "1 month") +
     scale_y_continuous(breaks = seq(1.5, length(levels(df$name))+0.5, 1), labels = levels(df$name)) +
     theme(axis.text.x = element_text(angle = 90)) +
     xlab("Date") +
     ylab("Names")
+
+  return(p)
 }
